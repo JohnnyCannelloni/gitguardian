c482d122d33a1a48e1a6677ddc6c0879a96476f1
JohnnyCannelloni <yoni.edelstein@gmail.com>
2025-06-04 18:46:46 +0300

    initial repo layout

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/JohnnyCannelloni/gitguardian
+
+go 1.24.2
---
+++ main.go
@@ -0,0 +1,8 @@
+// main.go
+package main
+
+import "github.com/JohnnyCannelloni/gitguardian/cmd"
+
+func main() {
+	cmd.Execute()
+}
